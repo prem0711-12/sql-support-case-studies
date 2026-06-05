538bb2065f1752baaa4f9c34da2cf52f09ac17cf
prem0711-12 <premrathod1416@gmail.com>
2026-06-05 09:11:57 +0530

    Create solution.sql

---
+++ Case 01 Find All Orders With Customer Details/solution.sql
@@ -0,0 +1,9 @@
+SELECT 	c.customer_id,
+		c.first_name,
+        c.country,
+        o.order_id,
+        o.order_status
+FROM customers c  
+INNER JOIN orders o 
+	on c.customer_id = o.customer_id
+WHERE o.order_id = 5004;    
